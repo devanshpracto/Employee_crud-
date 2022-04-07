cc18ff743c9d82ac9625f4141aa296eaee032574
devanshpracto <devansh.srivastava@practo.com>
2022-04-07 13:30:24 +0530

    CRUD completed

---
+++ src/Controller/EmployeeController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employee;
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class EmployeeController extends AbstractController
+{
+    private $employeeRepository;
+    private $emp;
+    public function __construct(EmployeeRepository $employeeRepository, EntityManagerInterface $emp)
+    {
+        $this->employeeRepository =$employeeRepository;
+        $this->emp=$emp;
+    }
+
+
+
+    #[Route('/employee', name: 'app_employee',methods:'GET')]
+    public function getAll() : Response
+    {
+        $employee = $this->employeeRepository->findAll();
+        // dd($employee);
+
+
+        if(!$employee){
+            return $this->json("No entry exists");
+        }
+        foreach($employee as $em){
+            $data[]=[
+                'id'=> $em->getId(),
+                'name'=> $em->getName(),
+                'Contact' => $em->getContact(),
+                'Address'=>  $em->getAddress() ,
+                'salary'=>  $em->getSalary(),
+                'Designation' => $em->getDesignation()
+            ];
+        }
+        return $this->json($data);
+    }
+
+
+
+    #[Route('/employee/{id}', name: 'app_employee_id',methods:'GET')]
+    public function getById($id) : Response
+    {   
+        $em = $this->employeeRepository->find($id);
+        if(!$em){
+            return $this->json("No entry for given id exists");
+        }
+       
+            $data=[
+                'id'=> $em->getId(),
+                'name'=> $em->getName(),
+                'Contact' => $em->getContact(),
+                'Address'=>  $em->getAddress() ,
+                'salary'=>  $em->getSalary(),
+                'Designation' => $em->getDesignation()
+            ];
+        
+        return $this->json($data);
+    }
+
+    #[Route('/employee', name: 'app_create',methods:'POST')]
+    public function create(Request $request) : Response
+    {
+        $para = json_decode($request->getContent(),true);
+        // dd($para);
+
+        $employee = new Employee;
+         $employee->setName($para['name'])
+         ->setContact($para['Contact'])
+         ->setAddress($para['Address'])
+         ->setSalary($para['salary'])
+         ->setDesignation($para['Designation']);
+
+        $this->emp->persist($employee);
+        $this->emp->flush();
+
+        return $this->json('Inserted Successfully');
+
+    }
+  
+
+
+    #[Route('/employee/{id}', name: 'app_delete',methods:'DELETE')]
+    public function delete($id) : Response
+    {
+        
+
+        $em = $this->employeeRepository->find($id);
+        
+        // $employee = new Employee;
+        if(!$em){
+            return $this->json("No entry for given id exists");
+        }
+        
+
+        $this->emp->remove($em);
+        $this->emp->flush();
+
+        return $this->json('Deleted Successfully');
+
+    }
+    #[Route('/employee/{id}', name: 'app_update',methods:'PUT')]
+    public function update(Request $request,$id) : Response
+    {
+        $para = json_decode($request->getContent(),true);
+
+        $em = $this->employeeRepository->find($id);
+        
+        // $employee = new Employee;
+        if(!$em){
+            return $this->json("No entry for given id exists");
+        }
+         $em->setName($para['name'])
+         ->setContact($para['Contact'])
+         ->setAddress($para['Address'])
+         ->setSalary($para['salary'])
+         ->setDesignation($para['Designation']);
+
+        $this->emp->persist($em);
+        $this->emp->flush();
+        return $this->json('Updated Successfully');
+
+    }
+}
---
+++ src/Entity/Employee.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmployeeRepository::class)]
+class Employee
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private $Contact;
+
+    #[ORM\Column(type: 'text')]
+    private $Address;
+
+    #[ORM\Column(type: 'integer')]
+    private $salary;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Designation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->Contact;
+    }
+
+    public function setContact(string $Contact): self
+    {
+        $this->Contact = $Contact;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->Address;
+    }
+
+    public function setAddress(string $Address): self
+    {
+        $this->Address = $Address;
+
+        return $this;
+    }
+
+    public function getSalary(): ?int
+    {
+        return $this->salary;
+    }
+
+    public function setSalary(int $salary): self
+    {
+        $this->salary = $salary;
+
+        return $this;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->Designation;
+    }
+
+    public function setDesignation(string $Designation): self
+    {
+        $this->Designation = $Designation;
+
+        return $this;
+    }
+}
